1103604bcd614995c7fc0c7a793234bd078d9cbb
kishan kumawat <kishankumawat1271@gamil.com>
2025-02-22 12:11:24 +0530

    changes1

--- backend/server.js
+++ backend/server.js
@@ -18,12 +18,12 @@ app.use(express.json())
   {
     orgin:"https://frontend-theta-eosin-95.vercel.app",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"]
+    // methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    // allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"]
   }
  
  ))
- app.options("*", cors());
+//  app.options("*", cors());
 
 
 
